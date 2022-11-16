7a754c5d6048b66a73f4c9baae79a1360ed50414
Felipe H. Y <felipehideo@hotmail.com>
2022-11-16 10:15:43 -0300

    env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 async function bootstrap() {
-   const httpsOptions = {
+  const httpsOptions = {
     key: fs.readFileSync('./secrets/key.pem'),
     cert: fs.readFileSync('./secrets/certificate.pem'),
   };
-  const app = await NestFactory.create(AppModule,{ httpsOptions } );
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.enableCors({
     origin: [
@@ -18,7 +22,7 @@ async function bootstrap() {
       'https://192.168.1.104:8100',
     ],
   });
-  await app.listen(3000, '192.168.1.104').then(() => {
+  await app.listen(3000, process.env.SERVIDOR_IP).then(() => {
     console.log('aplication started');
   });
 }
